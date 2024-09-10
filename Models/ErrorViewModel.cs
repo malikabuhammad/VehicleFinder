3f37931eb9fdb4fa6c829bbab5689ff86aba5b81
malikabuhammad <peace.twoo1@gmail.com>
2024-09-10 20:10:05 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
