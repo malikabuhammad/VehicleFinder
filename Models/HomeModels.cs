8b3b81b8616ddaa758ed5f34e898842ecfe29053
malikabuhammad <peace.twoo1@gmail.com>
2024-09-10 21:34:21 +0300

    Home models

---
+++ Models/HomeModels.cs
@@ -0,0 +1,28 @@
+﻿namespace VehicleFinder.Models
+{
+    public class HomeModels
+    {
+
+        // Get All Makes: returns json with this structure 
+        public class Make
+        {
+            public int MakeId { get; set; }
+            public string MakeName { get; set; }
+        }
+        //Get Vehicle Types for Make by Id:
+        public class VehicleType
+        {
+            public int VehicleTypeId { get; set; }
+            public string VehicleTypeName { get; set; }
+        }
+        //Get Vehicle Types for Make by Id:
+        public class VechicleModel
+        {
+            public int Make_ID { get; set; }
+            public string Make_Name { get; set; }
+            public int Model_ID { get; set; }
+            public string Model_Name { get; set; }
+        }
+
+    }
+}
